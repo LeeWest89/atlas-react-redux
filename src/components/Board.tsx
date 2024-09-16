89d8b0c2ebb24f8f703681964084e2534ab6ee48
LeeWest89 <lee.west@atlasschool.com>
2024-09-16 13:06:12 -0500

    DeleteListButton added looks good

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,9 @@
+import List from "./List";
+
 export default function Board() {
   return (
-    <div className="min-h-[43.45rem]"></div>
+    <div className="min-h-[43.45rem]">
+      <List />
+    </div>
   );
 };
---
+++ src/components/List.tsx
@@ -0,0 +1,9 @@
+import DeleteListButton from "./DeleteListButton";
+
+export default function List() {
+  return (
+    <div className="group/list h-full min-w-96 p-4">
+      <DeleteListButton />
+    </div>
+  );
+};
