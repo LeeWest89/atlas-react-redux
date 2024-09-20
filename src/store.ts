00df3ceeec7d002a19dae151d9e68d55544ced65
LeeWest89 <lee.west@atlasschool.com>
2024-09-20 14:02:04 -0500

    I have made it persistant, I did have an error that was because of a Non-serialized value I found a work around with middleware

--- src/store.ts
+++ src/store.ts
@@ -2,19 +2,40 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import listReducer from "./slices/listsSlice";
 import cardsReducer from "./slices/cardsSlice";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const listsPersistConfig = {
+  key: 'lists',
+  storage,
+};
+
+const cardsPersistConfig = {
+  key: 'cards',
+  storage,
+};
+
+const persistedListReducer = persistReducer(listsPersistConfig, listReducer);
+const persistedCardsReducer = persistReducer(cardsPersistConfig, cardsReducer);
 
 const store = configureStore({
   reducer: {
-    list: listReducer,
-    cards: cardsReducer,
+    list: persistedListReducer,
+    cards: persistedCardsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    })
 });
 
-// These types are helpful for the typescript autocomplete
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-//Add types to dispatch and selector hooks
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 
