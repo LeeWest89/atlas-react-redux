d53c911a478b3a98f82de394c5df27408e31a400
LeeWest89 <lee.west@atlasschool.com>
2024-09-23 11:06:19 -0500

    Created unit test and setting up for continuous integration

--- src/components/__tests__/cardsSlice.test.ts
+++ src/components/__tests__/cardsSlice.test.ts
@@ -1,1 +1,120 @@
-//  Unit Test for createCard, deleteCard, clearBoard, and moveCard
+//  Unit Test for createCard, deleteCard, and clearBoard
+import {cardSlice, clearBoard, createCard, deleteCard } from "../../slices/cardsSlice";
+import { expect, test } from "vitest";
+
+// createCard Test
+test("creates card correctly", () => {
+  const initialState = { items: [] };
+  const action = createCard({
+    listId: "1",
+    title: "Card One",
+    text: "Text of card one",
+    index: 0,
+  });
+  const state = cardSlice.reducer(initialState, action);
+
+  expect(state).toEqual({
+    items: [{ id: expect.any(String), listId: "1", title: "Card One", text: "Text of card one", index: 0 }],
+  });
+});
+
+test("creates cards correctly on multiple lists", () => {
+  const initialState = { items: [] };
+  let state = cardSlice.reducer(initialState, createCard({
+    listId: "1",
+    title: "Card One",
+    text: "Text of card one",
+    index: 0,
+  }));
+
+  expect(state).toEqual({
+    items: [{ id: expect.any(String), listId: "1", title: "Card One", text: "Text of card one", index: 0 }],
+  });
+
+  state = cardSlice.reducer(state, createCard({
+    listId: "2",
+    title: "Card Two",
+    text: "Text of card two",
+    index: 0,
+  }));
+
+  expect(state).toEqual({
+    items: [
+      { id: expect.any(String), listId: "1", title: "Card One", text: "Text of card one", index: 0 },
+      { id: expect.any(String), listId: "2", title: "Card Two", text: "Text of card two", index: 0 },
+    ],
+  });
+
+  state = cardSlice.reducer(state, createCard({
+    listId: "3",
+    title: "Card Three",
+    text: "Text of card three",
+    index: 0,
+  }));
+
+  expect(state).toEqual({
+    items: [
+      { id: expect.any(String), listId: "1", title: "Card One", text: "Text of card one", index: 0 },
+      { id: expect.any(String), listId: "2", title: "Card Two", text: "Text of card two", index: 0 },
+      { id: expect.any(String), listId: "3", title: "Card Three", text: "Text of card three", index: 0 },
+    ],
+  });
+});
+
+// deleteCard Test
+test("test if card is deleted correctly", () => {
+  const initialState = {
+    items: [{
+      id: "card 1",
+      listId: "1",
+      title: "Card One",
+      text: "Text of card one",
+      index: 0,
+    }],
+  };
+
+  const action = deleteCard("card 1");
+  const state = cardSlice.reducer(initialState, action);
+
+  expect(state).toEqual({ items: [] });
+});
+
+test("test if card is deleted correctly if there is more than one card", () => {
+  const initialState = {
+    items: [
+      { id: "card 1", listId: "1", title: "Card One", text: "Text of card one", index: 0 },
+      { id: "card 2", listId: "1", title: "Card Two", text: "Text of card two", index: 1 },
+      { id: "card 3", listId: "1", title: "Card Three", text: "Text of card three", index: 2 },
+    ],
+  };
+
+  let state = cardSlice.reducer(initialState, deleteCard("card 1"));
+
+  expect(state).toEqual({
+    items: [
+      { id: "card 2", listId: "1", title: "Card Two", text: "Text of card two", index: 1 },
+      { id: "card 3", listId: "1", title: "Card Three", text: "Text of card three", index: 2 },
+    ],
+  });
+
+  state = cardSlice.reducer(state, deleteCard("card 2"));
+
+  expect(state).toEqual({
+    items: [{ id: "card 3", listId: "1", title: "Card Three", text: "Text of card three", index: 2 }],
+  });
+});
+
+// clearBoard Test
+test("clearBoard should remove all list and cards", () => {
+  const initialState = {
+    items: [
+      { id: "card 1", listId: "1", title: "Card One", text: "Text of card one", index: 0 },
+      { id: "card 2", listId: "1", title: "Card Two", text: "Text of card two", index: 1 },
+      { id: "card 3", listId: "2", title: "Card Three", text: "Text of card three", index: 0 },
+    ],
+  };
+  const action = clearBoard();
+  const state = cardSlice.reducer(initialState, action);
+
+  expect(state).toEqual({ items: [] });
+});
--- src/components/__tests__/listsSlice.test.ts
+++ src/components/__tests__/listsSlice.test.ts
@@ -1,1 +1,79 @@
 // Unit test for addList, deleteList, addCard, and clearBoard
+import { listSlice, addList, addCard, deleteList, clearBoard } from "../../slices/listsSlice";
+import { expect, test } from "vitest";
+
+// Initial state
+const initialState = { items: [] };
+
+// addList Test
+test("addList should add a list", () => {
+  const action = addList({ title: "List One" });
+  const state = listSlice.reducer(initialState, action);
+  
+  expect(state).toEqual({
+    items: [{ id: expect.any(String), title: "List One", cardId: [] }],
+  });
+});
+
+test("addList should add 3 lists", () => {
+  let state = listSlice.reducer(initialState, addList({ title: "List One" }));
+  state = listSlice.reducer(state, addList({ title: "List Two" }));
+  state = listSlice.reducer(state, addList({ title: "List Three" }));
+
+  expect(state).toEqual({
+    items: [
+      { id: expect.any(String), title: "List One", cardId: [] },
+      { id: expect.any(String), title: "List Two", cardId: [] },
+      { id: expect.any(String), title: "List Three", cardId: [] },
+    ],
+  });
+});
+
+// deleteList Test
+test("deleteList should delete a list", () => {
+  const intialState = { items: [{ id: "1", title: "List one", cardId: [] }] };
+  const action = deleteList("1");
+  const state = listSlice.reducer(intialState, action);
+  
+  expect(state).toEqual({ items: [] });
+});
+
+test("deleteList should delete the list with id of 1 only", () => {
+  const intialState = {
+    items: [
+      { id: "1", title: "List one", cardId: [] },
+      { id: "2", title: "List two", cardId: [] },
+    ],
+  };
+  const action = deleteList("1");
+  const state = listSlice.reducer(intialState, action);
+
+  expect(state).toEqual({
+    items: [{ id: "2", title: "List two", cardId: [] }],
+  });
+});
+
+// addCard Test
+test("addCard should add a card to the list", () => {
+  const intialState = { items: [{ id: "1", title: "List one", cardId: [] }] };
+  const action = addCard({ lists: "1", cards: "Card One" });
+  const state = listSlice.reducer(intialState, action);
+  
+  expect(state).toEqual({
+    items: [{ id: "1", title: "List one", cardId: ["Card One"] }],
+  });
+});
+
+// clearBoard Test
+test("clearBoard should remove all lists", () => {
+  const intialState = {
+    items: [
+      { id: "1", title: "List one", cardId: [] },
+      { id: "2", title: "List two", cardId: [] },
+    ],
+  };
+  const action = clearBoard();
+  const state = listSlice.reducer(intialState, action);
+  
+  expect(state).toEqual({ items: [] });
+});
