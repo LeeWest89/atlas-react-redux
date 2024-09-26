92247f74d564653feacbc2355b53535be8406571
LeeWest89 <lee.west@atlasschool.com>
2024-09-26 18:50:30 -0500

    fixed the mistake that was causing the card to not be deleted onClick, it was do to draggable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,7 @@
-import { useDispatch } from "react-redux";
-import { deleteCard } from "../slices/cardsSlice";
 import DeleteCardButton from "./DeleteCardButton";
 import {CSS} from '@dnd-kit/utilities';
 import {useDraggable} from '@dnd-kit/core';
+import { useState } from 'react';
 
 interface CardProps {
   card: {
@@ -13,26 +12,22 @@ interface CardProps {
 }
 
 export default function Card({ card }: CardProps) {
-
-  const dispatch = useDispatch();
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
 
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: card.id,
+    disabled: isButtonHovered,
   });
 
   const style = {
     transform: CSS.Translate.toString(transform),
   }
 
-  const handleDeleteCard = () => {
-    dispatch(deleteCard(card.id));
-  };
-
   return (
     <div ref={setNodeRef} style={style} {...listeners} {...attributes} className="card group/card m-3 flex min-h-24 w-full max-w-96 flex-col items-center rounded bg-off-white-light px-4 py-2 text-blue">
       <h5 className="my-2 flex w-full items-end justify-between text-xl font-black">
         <span>{card.title}</span>
-        <DeleteCardButton onClick={handleDeleteCard} />
+        <DeleteCardButton cardId={card.id} onHover={setIsButtonHovered} />
       </h5>
       <p className="mt-0 text-left">{card.text}</p>
     </div>
--- src/components/DeleteCardButton.tsx
+++ src/components/DeleteCardButton.tsx
@@ -1,14 +1,25 @@
-import DeleteCard from "../images/DeleteCard.svg";
+import { useDispatch } from 'react-redux';
+import { deleteCard } from '../slices/cardsSlice';
+import DeleteCard from '../images/DeleteCard.svg';
 
 interface DeleteCardProps {
-  onClick: () => void;
+  cardId: string;
+  onHover: (isHovered: boolean) => void;
 }
 
-export default function DeleteCardButton({ onClick }: DeleteCardProps) {
+export default function DeleteCardButton({ cardId, onHover }: DeleteCardProps) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteCard(cardId));
+  };
+
   return (
     <button
       className="justify-center hidden w-[20px] group-hover/card:block h-[20px]"
-      onClick={onClick}
+      onClick={handleDelete}
+      onMouseEnter={() => onHover(true)}
+      onMouseLeave={() => onHover(false)}
     >
       <img src={DeleteCard} alt="Delete Card"></img>
     </button>
